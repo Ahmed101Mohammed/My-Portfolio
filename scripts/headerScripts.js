ce45d2df4d41e6acc177eae36c792a67d490b6ec
Ahmed101Mohammed <ahmedmohamedal3adl@gmail.com>
2024-04-15 03:48:16 +0200

    Delete the two sepreted js files ('dom','event') and replace them with headerScripts file.

--- scripts/headerScripts.js
+++ scripts/headerScripts.js
@@ -22,3 +22,20 @@ const setClassToPageHeader = (className)=>
 // Global Run:
 moveHireMeButton();
 moveNavLinks();
+
+let lastKnownYPosition = window.scrollY;
+document.addEventListener("scroll", ()=>
+{
+    let newYPositon = window.scrollY;
+    if((lastKnownYPosition - newYPositon) < -150)
+    {
+        setClassToPageHeader("disappear-header");
+        lastKnownYPosition = newYPositon;
+    }
+    else if((lastKnownYPosition - newYPositon) > 150)
+    {
+        setClassToPageHeader("appear-header");
+        lastKnownYPosition = newYPositon;
+    }
+    
+})
